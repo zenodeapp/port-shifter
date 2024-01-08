6b0d19ec19f9c4762f4f39e4b8b47a63c14d2c8d
zenodeapp <zenodeapp@gmail.com>
2024-01-08 09:02:38 +0100

    Altered for Namada v0.28.2

--- quick-shift.sh
+++ quick-shift.sh
@@ -26,31 +26,15 @@ RPC_LADDR=26657
 PPROF_LADDR=6060
 P2P_LADDR=26656
 PROMETHEUS_LISTEN_ADDR=26660
-
-# App.toml default ports
-API_ADDRESS=1317
-ROSETTA_ADDRESS=8080
-GRPC_ADDRESS=9090
-GRPC_WEB_ADDRESS=9091
-JSON_RPC_ADDRESS=8545
-JSON_RPC_WS_ADDRESS=8546
-JSON_RPC_METRICS_ADDRESS=6065
+ORACLE_RPC_ENDPOINT=8545
 
 # Create a backup
 cp $CONFIG_PATH/config.toml $CONFIG_PATH/config.toml.bak
-cp $CONFIG_PATH/app.toml $CONFIG_PATH/app.toml.bak
 
 # Shift ports
 sed -i "/^proxy_app/ s/\(proxy_app = \".*\):\([0-9]*\)/\1:$((PROXY_APP + INCREMENT))/" $CONFIG_PATH/config.toml
-sed -i "/^\[rpc\]/,/\[.*\]/ s/\(laddr = \".*\):\([0-9]*\)/\1:$((RPC_LADDR + INCREMENT))/" $CONFIG_PATH/config.toml
+sed -i "/^\[ledger.cometbft.rpc\]/,/\[.*\]/ s/\(laddr = \".*\):\([0-9]*\)/\1:$((RPC_LADDR + INCREMENT))/" $CONFIG_PATH/config.toml
 sed -i "/^pprof_laddr/ s/\(pprof_laddr = \".*\):\([0-9]*\)/\1:$((PPROF_LADDR + INCREMENT))/" $CONFIG_PATH/config.toml
-sed -i "/^\[p2p\]/,/\[.*\]/ s/\(laddr = \".*\):\([0-9]*\)/\1:$((P2P_LADDR + INCREMENT))/" $CONFIG_PATH/config.toml
+sed -i "/^\[ledger.cometbft.p2p\]/,/\[.*\]/ s/\(laddr = \".*\):\([0-9]*\)/\1:$((P2P_LADDR + INCREMENT))/" $CONFIG_PATH/config.toml
 sed -i "/^prometheus_listen_addr/ s/\(prometheus_listen_addr = \".*\):\([0-9]*\)/\1:$((PROMETHEUS_LISTEN_ADDR + INCREMENT))/" $CONFIG_PATH/config.toml
-sed -i "/^\[api\]/,/\[.*\]/ s/\(address = \".*\):\([0-9]*\)/\1:$((API_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-sed -i "/^\[rosetta\]/,/\[.*\]/ s/\(address = \".*\):\([0-9]*\)/\1:$((ROSETTA_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-sed -i "/^\[grpc\]/,/\[.*\]/ s/\(address = \".*\):\([0-9]*\)/\1:$((GRPC_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-sed -i "/^\[grpc-web\]/,/\[.*\]/ s/\(address = \".*\):\([0-9]*\)/\1:$((GRPC_WEB_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-# Make sure to run the three below together and do not comment out the last two, since the first will at first overwrite all three.
-sed -i "/^\[json-rpc\]/,/\[.*\]/ s/\(address = \".*\):\([0-9]*\)/\1:$((JSON_RPC_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-sed -i "/^\[json-rpc\]/,/\[.*\]/ s/\(ws-address = \".*\):\([0-9]*\)/\1:$((JSON_RPC_WS_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-sed -i "/^\[json-rpc\]/,/\[.*\]/ s/\(metrics-address = \".*\):\([0-9]*\)/\1:$((JSON_RPC_METRICS_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
+sed -i "/^oracle_rpc_endpoint/ s/\(oracle_rpc_endpoint = \".*\):\([0-9]*\)/\1:$((ORACLE_RPC_ENDPOINT + INCREMENT))/" $CONFIG_PATH/config.toml
--- shift-wizard.sh
+++ shift-wizard.sh
@@ -7,18 +7,18 @@
 # let the user customize each port individually. If you do not trust
 # this wizard or prefer a simpler variant, use quick-shift.sh instead.
 
-read -p "What's the path to the config files? (example: ~/.gaia/config): " CONFIG_PATH
+read -p "What's the path to the config.toml file? (example: ~/.local/share/namada/public-testnet-15.0dacadb8d663): " CONFIG_PATH
 CONFIG_PATH=$(eval echo "$CONFIG_PATH" | sed 's/\/$//') # expand tilde and remove trailing slash
 
 # Check if CONFIG_PATH is empty
 if [ -z "$CONFIG_PATH" ]; then
-    echo "Error: The path to the config files cannot be empty."
+    echo "Error: The path to the config file cannot be empty."
     exit 1
 fi
 
 # Check if config files exist
-if [ ! -f "$CONFIG_PATH/config.toml" ] || [ ! -f "$CONFIG_PATH/app.toml" ]; then
-    echo "Error: One or both of the config files ($CONFIG_PATH/config.toml or $CONFIG_PATH/app.toml) not found."
+if [ ! -f "$CONFIG_PATH/config.toml" ]; then
+    echo "Error: config file $CONFIG_PATH/config.toml not found."
     exit 1
 fi
 
@@ -50,7 +50,6 @@ prompt_for_port() {
 
 # backup
 cp $CONFIG_PATH/config.toml $CONFIG_PATH/config.toml.bak
-cp $CONFIG_PATH/app.toml $CONFIG_PATH/app.toml.bak
 
 # Config.toml default ports
 PROXY_APP=26658
@@ -58,6 +57,7 @@ RPC_LADDR=26657
 PPROF_LADDR=6060
 P2P_LADDR=26656
 PROMETHEUS_LISTEN_ADDR=26660
+ORACLE_RPC_ENDPOINT=8545
 
 # Ask for each port value if CUSTOM is "y"
 if [ "$CUSTOM" = "y" ]; then
@@ -66,44 +66,14 @@ if [ "$CUSTOM" = "y" ]; then
     PPROF_LADDR=$(prompt_for_port "pprof_laddr" $PPROF_LADDR)
     P2P_LADDR=$(prompt_for_port "[p2p] laddr" $P2P_LADDR)
     PROMETHEUS_LISTEN_ADDR=$(prompt_for_port "prometheus_listen_addr" $PROMETHEUS_LISTEN_ADDR)
+    ORACLE_RPC_ENDPOINT=$(prompt_for_port "oracle_rpc_endpoint" $ORACLE_RPC_ENDPOINT)
 fi
 
 sed -i "/^proxy_app/ s/\(proxy_app = \".*\):\([0-9]*\)/\1:$((PROXY_APP + INCREMENT))/" $CONFIG_PATH/config.toml
-sed -i "/^\[rpc\]/,/\[.*\]/ s/\(laddr = \".*\):\([0-9]*\)/\1:$((RPC_LADDR + INCREMENT))/" $CONFIG_PATH/config.toml
+sed -i "/^\[ledger.cometbft.rpc\]/,/\[.*\]/ s/\(laddr = \".*\):\([0-9]*\)/\1:$((RPC_LADDR + INCREMENT))/" $CONFIG_PATH/config.toml
 sed -i "/^pprof_laddr/ s/\(pprof_laddr = \".*\):\([0-9]*\)/\1:$((PPROF_LADDR + INCREMENT))/" $CONFIG_PATH/config.toml
-sed -i "/^\[p2p\]/,/\[.*\]/ s/\(laddr = \".*\):\([0-9]*\)/\1:$((P2P_LADDR + INCREMENT))/" $CONFIG_PATH/config.toml
+sed -i "/^\[ledger.cometbft.p2p\]/,/\[.*\]/ s/\(laddr = \".*\):\([0-9]*\)/\1:$((P2P_LADDR + INCREMENT))/" $CONFIG_PATH/config.toml
 sed -i "/^prometheus_listen_addr/ s/\(prometheus_listen_addr = \".*\):\([0-9]*\)/\1:$((PROMETHEUS_LISTEN_ADDR + INCREMENT))/" $CONFIG_PATH/config.toml
+sed -i "/^oracle_rpc_endpoint/ s/\(oracle_rpc_endpoint = \".*\):\([0-9]*\)/\1:$((ORACLE_RPC_ENDPOINT + INCREMENT))/" $CONFIG_PATH/config.toml
 
 echo "Ports have been replaced in $CONFIG_PATH/config.toml"
-echo ""
-
-# App.toml default ports
-API_ADDRESS=1317
-ROSETTA_ADDRESS=8080
-GRPC_ADDRESS=9090
-GRPC_WEB_ADDRESS=9091
-JSON_RPC_ADDRESS=8545
-JSON_RPC_WS_ADDRESS=8546
-JSON_RPC_METRICS_ADDRESS=6065
-
-# Ask for each port value if CUSTOM is "y"
-if [ "$CUSTOM" = "y" ]; then
-    API_ADDRESS=$(prompt_for_port "[api] address" $API_ADDRESS)
-    ROSETTA_ADDRESS=$(prompt_for_port "[rosetta] address" $ROSETTA_ADDRESS)
-    GRPC_ADDRESS=$(prompt_for_port "[grpc] address" $GRPC_ADDRESS)
-    GRPC_WEB_ADDRESS=$(prompt_for_port "[grpc-web] address" $GRPC_WEB_ADDRESS)
-    JSON_RPC_ADDRESS=$(prompt_for_port "[json-rpc] address" $JSON_RPC_ADDRESS)
-    JSON_RPC_WS_ADDRESS=$(prompt_for_port "[json-rpc] ws-address" $JSON_RPC_WS_ADDRESS)
-    JSON_RPC_METRICS_ADDRESS=$(prompt_for_port "[json-rpc] metrics-address" $JSON_RPC_METRICS_ADDRESS)
-fi
-
-sed -i "/^\[api\]/,/\[.*\]/ s/\(address = \".*\):\([0-9]*\)/\1:$((API_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-sed -i "/^\[rosetta\]/,/\[.*\]/ s/\(address = \".*\):\([0-9]*\)/\1:$((ROSETTA_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-sed -i "/^\[grpc\]/,/\[.*\]/ s/\(address = \".*\):\([0-9]*\)/\1:$((GRPC_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-sed -i "/^\[grpc-web\]/,/\[.*\]/ s/\(address = \".*\):\([0-9]*\)/\1:$((GRPC_WEB_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-# Make sure to run the three below together and do not comment out the last two, since the first will at first overwrite all three.
-sed -i "/^\[json-rpc\]/,/\[.*\]/ s/\(address = \".*\):\([0-9]*\)/\1:$((JSON_RPC_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-sed -i "/^\[json-rpc\]/,/\[.*\]/ s/\(ws-address = \".*\):\([0-9]*\)/\1:$((JSON_RPC_WS_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-sed -i "/^\[json-rpc\]/,/\[.*\]/ s/\(metrics-address = \".*\):\([0-9]*\)/\1:$((JSON_RPC_METRICS_ADDRESS + INCREMENT))/" $CONFIG_PATH/app.toml
-
-echo "Ports have been replaced in $CONFIG_PATH/app.toml"
